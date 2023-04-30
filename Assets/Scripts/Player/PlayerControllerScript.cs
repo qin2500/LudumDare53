5a430bbf8da69d9dd3bb4c732ac03e8ef910988a
Matthew Xie <mattx180@gmail.com>
2023-04-30 11:29:03 -0400

    add player speed modes

--- Assets/Scripts/Player/PlayerControllerScript.cs
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    public float speed;
+    public float trotSpeed;
+    public float gallopSpeed;
 
+    private bool gallop;
     private List<Cow> cows;
     private Rigidbody2D rb;
     private Vector2 velocity;
@@ -20,15 +22,31 @@ public class PlayerControllerScript : MonoBehaviour
     {
         var inputH = Input.GetAxisRaw("Horizontal");
         var inputV = Input.GetAxisRaw("Vertical");
+
         velocity.x = inputH;
         velocity.y = inputV;
 
         velocity.Normalize();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            gallop = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            gallop= false;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = velocity * speed;
+        if (!gallop)
+        {
+            rb.velocity = velocity * trotSpeed;
+        }
+        else
+        {
+            rb.velocity = velocity * gallopSpeed;
+        }
     }
 
     public List<Cow> getCows()
