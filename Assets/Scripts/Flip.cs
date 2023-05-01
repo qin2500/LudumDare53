ae99731b6bf04ce8080309d317b81722440a09dd
qin2500 <52422134+qin2500@users.noreply.github.com>
2023-05-01 07:59:13 -0400

    Added sprite flippiing script

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public GameObject sprite;
+    public bool faceingRight;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.x > 0 && !faceingRight) flip();
+        else if (rb.velocity.x < 0 && faceingRight) flip();
+    }
+    private void flip()
+    {
+
+        faceingRight = !faceingRight;
+        Vector3 scaler = sprite.transform.localScale;
+        scaler.x *= -1;
+        sprite.transform.localScale = scaler;
+    }
+}
