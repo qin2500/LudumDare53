96e14b5a19ccba11aba9dd028cb5b3e6c9f8896e
qin2500 <52422134+qin2500@users.noreply.github.com>
2023-05-01 13:23:41 -0400

    Added lime Cow

---
+++ Assets/Scripts/Cow/Cow.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string name;
+    public int id;
+}
