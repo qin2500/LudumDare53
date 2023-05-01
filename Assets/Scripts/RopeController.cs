02ab9966ef9ebf64dfb552e6ca3579cebb810953
qin2500 <52422134+qin2500@users.noreply.github.com>
2023-05-01 15:02:23 -0400

    Fixed more bugs with the cows

--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -31,6 +31,7 @@ public class RopeController : MonoBehaviour
         {
             player.transform.GetChild(0).GetComponent<PatchLassoController>().ropping = false;
             cow.GetComponent<CowBehaviour>().wanderState();
+            cow.tag = "Cow";
             distroy();
         }
     }
